832d11b8af0ac2a19afc179723401fd49f91835e
Calum <contact@mineblock11.dev>
2024-04-20 21:14:50 +0100

    Add build files for pi, super annoying...

---
+++ src/api/public/_next/static/hl8pfC7BwtPNOpBGWzR_R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-73c4bb65cd80f43b.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
