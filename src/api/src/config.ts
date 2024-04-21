3f4a0542da1fcdcb317c978ab0997197bbcbdcfe
Calum <contact@mineblock11.dev>
2024-04-21 14:20:54 +0100

    utf moment

--- src/api/src/config.ts
+++ src/api/src/config.ts
@@ -1,4 +1,4 @@
 import { readFileSync } from "node:fs";
 
 
-export const config = JSON.parse(readFileSync(".secrets.json", "utf-8"));
+export const config = JSON.parse(readFileSync(".secrets.json", "utf-16"));
